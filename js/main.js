98f329b81c3bb53b01dfa06f86f6cfb5d3501bdd
Jocelyn Kong <jocelynk@andrew.cmu.edu>
2012-09-30 21:41:13 -0400

    basic add calendar, add events, get freebusy

--- js/main.js
+++ js/main.js
@@ -2,8 +2,29 @@
 $(document).ready(function() {
 	
 });
+ 
+function init() { 
+    gapi.auth.init(checkAuth); 
+} 
 
-function load() {
+function checkAuth() { 
+    var config = {
+        'client_id': '1054661617958.apps.googleusercontent.com',
+        'scope': 'https://www.googleapis.com/auth/calendar',
+        'response_type': 'token'
+    };
+    setTimeout(function() { 
+        gapi.auth.authorize(
+        config, function() {
+            console.log('login complete');
+           // load();
+            console.log(gapi.auth.getToken());
+        }); 
+    }, 1); 
+} 
+
+//Add Events to Calendar
+function asdf() {
     gapi.client.setApiKey('AIzaSyDiwVKBJwrbJFtrxqjenl7u9fk5eVMoMJw');
     var resource = {
         "summary":"My Summary",
@@ -19,9 +40,9 @@ function load() {
     'summary': 'Appointment',
     'location': 'Somewhere',
     'start': {
-    'dateTime': '2012-09-03T10:00:00.000-07:00'},
+    'dateTime': '2012-09-10T10:00:00.000-07:00'},
     'end': {
-    'dateTime': '2012-09-03T10:25:00.000-07:00'}
+    'dateTime': '2012-09-10T10:25:00.000-07:00'}
     };
     gapi.client.load('calendar', 'v3', function() {
         var req = gapi.client.calendar.events.insert({
@@ -39,23 +60,52 @@ function load() {
         });
     });
 }
- 
-function init() { 
-    gapi.auth.init(checkAuth); 
-} 
+//make Calendar
+function qwer(){
+    gapi.client.setApiKey('AIzaSyDiwVKBJwrbJFtrxqjenl7u9fk5eVMoMJw');
 
-function checkAuth() { 
-    var config = {
-        'client_id': '1054661617958.apps.googleusercontent.com',
-        'scope': 'https://www.googleapis.com/auth/calendar',
-        'response_type': 'token'
-    };
-    setTimeout(function() { 
-        gapi.auth.authorize(
-        config, function() {
-            console.log('login complete');
-            load();
-            console.log(gapi.auth.getToken());
-        }); 
-    }, 1); 
-} 
+calendar = {
+    'summary': 'calendarSummary',
+    'timeZone': 'America/New_York'
+}
+ gapi.client.load('calendar', 'v3', function() {
+ var req = gapi.client.calendar.calendars.insert({'resource':calendar});
+ req.execute(function(resp) {
+            console.log(resp);
+			console.log(resp.id);
+            if (resp.id){
+                alert("Event was successfully added to the calendar!");
+            } else{
+                alert("An error occurred. Please try again later.")
+            }
+       
+        });
+		});
+}
+
+function load() {
+    gapi.client.setApiKey('AIzaSyDiwVKBJwrbJFtrxqjenl7u9fk5eVMoMJw');
+	var data = {
+			 'timeMin': '2012-09-30T10:00:00.000-07:00',
+    'timeMax': '2012-10-02T10:25:00.000-07:00',
+				'items': [
+					{
+						'id': 'r5p25tev3v42r11u242u959ev8@group.calendar.google.com'
+					},{
+						'id': 'cq6omn9kt4uld6agu4dhevucm0@group.calendar.google.com'
+					}
+				]};
+		gapi.client.load('calendar', 'v3', function() {
+			var query = gapi.client.calendar.freebusy.query(data);
+			query.execute(function(resp) {
+            console.log(resp);
+			//console.log(resp.id);
+            if (resp){
+                alert("Freebusy was successfully added to the calendar!");
+            } else{
+                alert("An error occurred. Please try again later.")
+            }
+       
+        })
+		;});
+}
